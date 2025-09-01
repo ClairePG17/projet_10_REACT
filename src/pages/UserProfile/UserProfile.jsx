0eb67971f303ee8657cfed5d850c8de7e01ac764
Claire Prin-Guénon <pringuenon.claire@gmail.com>
2025-09-01 11:00:20 +0200

    implementation component CartTransaction

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchUserProfile } from '../../redux/reducers/authSlice'
+import CartTransaction from '../../components/CartTransaction/CartTransaction'
 
 export default function UserProfile() {
   const dispatch = useDispatch()
@@ -31,38 +32,25 @@ export default function UserProfile() {
           <button className="edit-button">Edit Name</button>
         </div>
         <h2 className="sr-only">Accounts</h2>
-          <section className="account">
-            <div className="account-content-wrapper">
-              <h3 className="account-title">Argent Bank Checking (x8349)</h3>
-              <p className="account-amount">$2,082.79</p>
-              <p className="account-amount-description">Available Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-              <button className="transaction-button">View transactions</button>
-            </div>
-          </section>
-          <section className="account">
-            <div className="account-content-wrapper">
-              <h3 className="account-title">Argent Bank Savings (x6712)</h3>
-              <p className="account-amount">$10,928.42</p>
-              <p className="account-amount-description">Available Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-              <button className="transaction-button">View transactions</button>
-            </div>
-          </section>
-          <section className="account">
-            <div className="account-content-wrapper">
-              <h3 className="account-title">Argent Bank Credit Card (x8349)</h3>
-              <p className="account-amount">$184.30</p>
-              <p className="account-amount-description">Current Balance</p>
-            </div>
-            <div className="account-content-wrapper cta">
-              <button className="transaction-button">View transactions</button>
-            </div>
-          </section>
-        </main>
-      </>
-    );
-  }
-  
+        <CartTransaction
+          title="Argent Bank Checking"
+          number="8349"
+          amount="2,082.79"
+          description="Available Balance"
+        />
+        <CartTransaction
+          title="Argent Bank Savings"
+          number="6712"
+          amount="10,928.42"
+          description="Available Balance"
+        />
+        <CartTransaction
+          title="Argent Bank Credit Card"
+          number="8349"
+          amount="184.30"
+          description="Current Balance"
+        />
+      </main>
+    </>
+  )
+}
